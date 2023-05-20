89a61bec56ca00bea01595e7b494fc77ea2fb676
Naima Malik <noma_m@hotmail.com>
2023-05-20 12:27:11 +0200

    Remove collapse and adjust svg blog

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,18 +8,8 @@ const Navigation = () => {
           <Link className="navbar-brand" to="/">
             Naima Malik Portfolio
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+
+          <div id="navbarNavAltMarkup">
             <div className="navbar-nav">
               <Link className="nav-link" to="/">
                 Home
