2521cbab4edc089bb7a6871361ac1f9cd508ecff
Naima Malik <noma_m@hotmail.com>
2023-05-31 19:36:02 +0200

    Change dark puple colour to lighter purple

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const Navigation = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-0">
         <motion.div
           className="container-fluid"
           initial={{ y: -250 }}
