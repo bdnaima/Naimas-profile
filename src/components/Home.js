b2a2cc03fd72caecf7bdb92661ab0195c3076fad
Naima Malik <noma_m@hotmail.com>
2024-04-29 17:21:44 +0200

    Change information

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,7 +36,7 @@ const Home = () => {
           transition={{ delay: 0.5, duration: 2 }}
         >
           <h2>Hello, I'm Naima</h2>
-          <h3>Frontend developer & Web designer</h3>
+          <h3>Frontend developer with UX/UI design experience</h3>
         </motion.div>
         <motion.div
           className="stack"
